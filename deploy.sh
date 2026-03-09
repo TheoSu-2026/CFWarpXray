baccbf88568a54c22d8c35848e3a70ce79e513ac
weijianguo <>
2026-03-09 17:48:33 +0800

    ≈

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,8 @@ OS=$(uname -s)
 DOCKER_BACKEND="${DOCKER_BACKEND:-desktop}"
 
 if [ "$OS" = "Darwin" ]; then
-    INSTALL_DIR="${INSTALL_DIR:-$HOME/CFWarpXray}"
+    # 默认拉取到用户文档目录：~/Documents/CFWarpXray
+    INSTALL_DIR="${INSTALL_DIR:-$HOME/Documents/CFWarpXray}"
 else
     INSTALL_DIR="${INSTALL_DIR:-/opt/CFWarpXray}"
 fi
